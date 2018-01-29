a95c1cf265042868fa678744ef75a5b4cfcc8bea
cody rivas <services4cody@protonmail.com>
2018-01-29 23:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 if (name === "Susan") {console.log("The name is correct") }
-
+else if(name === "Joe") {console.log("Oh that's right it is Joe") }
 else {console.log("Expected the name to be susan") }
 
 
